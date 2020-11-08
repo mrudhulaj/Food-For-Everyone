1da29986cd00ac050af16b909a50ba39e5eacb6a
midhulajzain <midhulajzain@gmail.com>
2020-11-08 22:19:32 +0530

    Home page UI changes ongoing

--- resources/views/homePage/home.blade.php
+++ resources/views/homePage/home.blade.php
@@ -1,7 +1,54 @@
 @extends('templates.main')
 @section('content')
 {{--  Main Content Begins  --}}
-<section class="carosal-area">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+	</ol>
+
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner">
+
+		<div class="item active">
+			<img src="{{url('images/home-carousel/mizoram.jpg')}}" alt="Mizoram" width="100%">
+			<div class="carousel-caption">
+				<h3>Los Angeles</h3>
+				<p>LA is always so much fun!</p>
+			</div>
+		</div>
+
+		<div class="item">
+			<img src="{{url('images/home-carousel/mizoram.jpg')}}" alt="Chennai" width="100%">
+			<div class="carousel-caption">
+				<h3>Chicago</h3>
+				<p>Thank you, Chicago!</p>
+			</div>
+		</div>
+	
+		<div class="item">
+			<img src="{{url('images/home-carousel/mizoram.jpg')}}" alt="Punjab" width="100%">
+			<div class="carousel-caption">
+				<h3>New York</h3>
+				<p>We love the Big Apple!</p>
+			</div>
+		</div>
+
+	</div>
+
+	<!-- Left and right controls -->
+	<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#myCarousel" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
+{{--  <section class="carosal-area" style="border: 2px solid red;">
 		<div class="container">
 				<div class="row">
 						<div class="col-md-12">
@@ -50,7 +97,8 @@
 						</div>
 				</div>
 		</div>
-</section>
+</section>  --}}
+
 <section class="our_activity">
 		<h2>OUR ACTIVITY</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor <br> incididunt ut labore et dolore magna aliqua. </p>
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -30,7 +30,7 @@
 											</div>
 											<div class="col-md-4 col-sm-4">
 													<div class="join-us">
-															<p><a href="#">Sign Up / Login Page</a></p>
+															<p><a href="#">Sign Up / Login</a></p>
 													</div>
 											</div>
 									</div>
@@ -73,8 +73,8 @@
 							<div class="row">
 									<div class="col-md-4 col-sm-12">
 											<div class="footer-charity-text">
-													<h2>HELP CHARITY</h2>
-													<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris </p>
+													<h2>Food For Everyone</h2>
+													<p>We are a non-profit organization helping restaurents and event planners to distribute their leftover food to the needy people.</p>
 													<hr>
 													<p><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></p>
 											</div>
@@ -85,11 +85,11 @@
 															<div class="footer-text one">
 																	<h3>RECENT POST</h3>
 																	<ul>
-																			<li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing Elit</a></li>
-																			<li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing </a></li>
-																			<li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing Elit</a></li>
-																			<li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing</a></li>
-																			<li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing Elit</a></li>
+																			<li><a href="#"><i class="material-icons">keyboard_arrow_right</i>Volunteers awarded</a></li>
+																			<li><a href="#"><i class="material-icons">keyboard_arrow_right</i>Support from Kerala Government</a></li>
+																			<li><a href="#"><i class="material-icons">keyboard_arrow_right</i>New restaurents added</a></li>
+																			<li><a href="#"><i class="material-icons">keyboard_arrow_right</i>Guide for event planners</a></li>
+																			<li><a href="#"><i class="material-icons">keyboard_arrow_right</i>How to minimise food wastage</a></li>
 																	</ul>
 															</div>
 													</div>
@@ -108,9 +108,9 @@
 															<div class="footer-text one">
 																	<h3>CONTACT US</h3>
 																	<ul>
-																			<li><a href="#"><i class="material-icons">location_on</i>1 Street, derby, FL 2147, USA</a></li>
-																			<li><a href="#"><i class="material-icons">email</i>dartthemes@gmail.com</a></li>
-																			<li><a href="#"><i class="material-icons">call</i>+123456789</a></li>
+																			<li><a href="#"><i class="material-icons">location_on</i>Emerald Building , Calicut , Kerala , India - 673005</a></li>
+																			<li><a href="#"><i class="material-icons">email</i>foodforeveryone@gmail.com</a></li>
+																			<li><a href="#"><i class="material-icons">call</i>+91 9998881111</a></li>
 																	</ul>
 															</div>
 													</div>
@@ -119,8 +119,13 @@
 							</div>
 					</div>
 					<div class="footer_bottom">
-							<p>Registered as FoodForEveryone &copy | All donations are tax-exempted as eligible under section 80G of the Income Tax
-                Act, 1961. </p>
+							<p>
+								Registered as FoodForEveryone &copy
+							</p>
+							<p style="margin: 0;margin-top: -40px;">
+								All donations are tax-exempted as eligible under section 80G of the Income Tax
+								Act, 1961. 
+							</p>
 					</div>
 			</footer>
 	</div>
