145a7efeae00d60f7b36760f4e6a513846e837ac
midhulajzain <midhulajzain@gmail.com>
2020-11-13 02:23:58 +0530

    causes multiple boxes UI design ongoing

--- resources/views/causes/causes.blade.php
+++ resources/views/causes/causes.blade.php
@@ -8,13 +8,15 @@
 
   .box{
     width: 420px;
-    height: 750px;
+    height: 725px;
   }
 
   .mainbox{
 		margin-bottom: 20px;
 		border-radius: 13px;
     box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.5);
+    display: inline-block;
+    margin-right: 50px;
    }
    
   .box .box-content h2 {
@@ -22,7 +24,7 @@
     font-family: "Roboto", sans-serif;
     font-size: 20px;
     font-weight: 700;
-    margin: 110px 0 15px;
+    margin: 100px 0 15px;
   }
 
   hr {
@@ -93,67 +95,9 @@
     <h2 style="margin-top: 0px;">We are expanding our reach!</h2>
     <p>We are continously trying to expand our reach to different areas where people need support to help build a better future.
 			<br>These are the current causes which we support currently.</p>
-	{{--  </section>
-    <section class="our_cuauses">
-      <div class="container">
-          <div class="row">
-              <div class="col-md-12">
-                  <div class="our_cuauses_single owl-carousel owl-theme">
-                      <div class="item">
-                          <img src="{{url('images/our_cuauses_one.jpg')}}">
-                          <div class="for_padding">
-                              <h2>FUTURE FOR CHILDREN</h2>
-                              <p style="height: 120px;">Join us in helping build our bright youngsters a school to showcase their skills and to make them a change and making the world a better place.</p>
-                              <div class="progress-text">
-                                  <p class="progress-top">50%</p>
-                                  <div class="progress">
-                                      <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%"></div>
-                                  </div>
-                                  <p class="progress-left">Raised: <span>1200 ₹</span></p>
-                                  <p class="progress-right">Goal: <span>2400 ₹</span></p>
-                              </div>
-                              <h2 class="borderes"><a href="#">DONATE NOW</a></h2>
-                          </div>
-                      </div>
-                      <div class="item">
-                          <img src="{{url('images/our_cuauses_two.jpg')}}">
-                          <div class="for_padding">
-                              <h2>CARE HOME FOR ELDERS</h2>
-                              <p style="height: 120px;">Let's not forget how we got here.Join us in building our elders a safe and protective home to assure them that kindness still exists.</p>
-                              <div class="progress-text">
-                                  <p class="progress-top">50%</p>
-                                  <div class="progress">
-                                      <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%"></div>
-                                  </div>
-                                  <p class="progress-left">Raised: <span>1200 ₹</span></p>
-                                  <p class="progress-right">Goal: <span>2400 ₹</span></p>
-                              </div>
-                              <h2 class="borderes"><a href="#">DONATE NOW</a></h2>
-                          </div>
-                      </div>
-                      <div class="item">
-                          <img src="{{url('images/our_cuauses_three.jpg')}}">
-                          <div class="for_padding">
-                              <h2>CHARITABLE HOSPITAL FOR ALL</h2>
-                              <p style="height: 120px;">The most helpless one's are the most disease prone one's.Malnutrition , unhygenic living conditions comes at a cost.But we help them recover for a better tommorow.Join us in helping this dream a possibility.</p>
-                              <div class="progress-text">
-                                  <p class="progress-top">50%</p>
-                                  <div class="progress">
-                                      <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%"></div>
-                                  </div>
-                                  <p class="progress-left">Raised: <span>1200 ₹</span></p>
-                                  <p class="progress-right">Goal: <span>2400 ₹</span></p>
-                              </div>
-                              <h2 class="borderes"><a href="#">DONATE NOW</a></h2>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </section>  --}}
 
-  <div class="container">
+  <div class="container" style="margin-top: 70px;margin-bottom: 70px">
+    {{--  Begin: To be repeated content  --}}
     <div class="box mainbox">
         <img src="{{url('images/our_cuauses_one.jpg')}}" style="border-top-left-radius: inherit;border-top-right-radius: inherit;">
         <div class="box-content" style="padding: 0 30px;">
@@ -168,8 +112,45 @@
                 <p class="progress-left">Raised: <span class="progress-amount">1200 ₹</span></p>
                 <p class="progress-right">Goal: <span class="progress-amount">2400 ₹</span></p>
             </div>
-            <h2 class="borderes" style="text-align: center"><a href="#">DONATE NOW</a></h2>
+            <h2 class="borderes" style="text-align: center;margin-top: 100px;"><a href="#">DONATE NOW</a></h2>
         </div>
     </div>
+    {{--  End: To be repeated content  --}}
+    {{-- Begin:  Sample repeated Items  --}}
+    <div class="box mainbox">
+      <img src="{{url('images/our_cuauses_one.jpg')}}" style="border-top-left-radius: inherit;border-top-right-radius: inherit;">
+      <div class="box-content" style="padding: 0 30px;">
+          <h2 style="text-align: center">FUTURE FOR CHILDREN</h2>
+          <hr>
+          <p class="box-p" style="height: 120px;">Join us in helping build our bright youngsters a school to showcase their skills and to make them a change and making the world a better place.</p>
+          <div class="progress-text">
+              <p class="progress-top">50%</p>
+              <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%;background-color: #01d262;"></div>
+              </div>
+              <p class="progress-left">Raised: <span class="progress-amount">1200 ₹</span></p>
+              <p class="progress-right">Goal: <span class="progress-amount">2400 ₹</span></p>
+          </div>
+          <h2 class="borderes" style="text-align: center;margin-top: 100px;"><a href="#">DONATE NOW</a></h2>
+      </div>
+  </div>
+  <div class="box mainbox">
+    <img src="{{url('images/our_cuauses_one.jpg')}}" style="border-top-left-radius: inherit;border-top-right-radius: inherit;">
+    <div class="box-content" style="padding: 0 30px;">
+        <h2 style="text-align: center">FUTURE FOR CHILDREN</h2>
+        <hr>
+        <p class="box-p" style="height: 120px;">Join us in helping build our bright youngsters a school to showcase their skills and to make them a change and making the world a better place.</p>
+        <div class="progress-text">
+            <p class="progress-top">50%</p>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%;background-color: #01d262;"></div>
+            </div>
+            <p class="progress-left">Raised: <span class="progress-amount">1200 ₹</span></p>
+            <p class="progress-right">Goal: <span class="progress-amount">2400 ₹</span></p>
+        </div>
+        <h2 class="borderes" style="text-align: center;margin-top: 100px;"><a href="#">DONATE NOW</a></h2>
+    </div>
+</div>
+    {{--  End : Sample repeated Items  --}}
   </div>
 @stop
