bed18c69d5079b431e43c006ae7b7fadfaa86a99
midhulajzain <midhulajzain@gmail.com>
2021-04-20 16:35:55 +0300

    General-2 : Default alert template updated.

--- resources/views/templates/alertSuccessOrErrorMessage.blade.php
+++ resources/views/templates/alertSuccessOrErrorMessage.blade.php
@@ -1,15 +1,15 @@
 @if(session('status'))
-<div class="alert alert-success alert-dismissable">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-    {{ session('status') }}
-</div>
+    <div class="alert alert-success alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        {{ session('status') }}
+    </div>
 @elseif(session('error-status'))
-<div class="alert alert-danger alert-dismissable">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-    {{ session('error-status') }}
-</div>
+    <div class="alert alert-danger alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        {{ session('error-status') }}
+    </div>
 @endif
